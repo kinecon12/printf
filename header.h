8e46412b5891349729ac42ad7b6ded5e267848ec
kinecon12 <kinecon12@gmail.com>
2023-10-17 15:07:36 -0700

    headerfile

---
+++ header.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/* Standard C library for header files */
+#include <stdarg.h>
+#include <unistd.h>
+
+void getc_ints(int num);
+int putchr(char z);
+int putt_intts(int integer);
+int  p_printf(const char *format, ...);
+int putts(char *string);
+int m_put(char *string);
+
+#endif /* HEADER.H file */
