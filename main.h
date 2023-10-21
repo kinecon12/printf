affa1945a378c844a086e1dee0b7a7b5efdd4220
kinecon12 <kinecon12@gmail.com>
2023-10-20 21:09:08 -0700

    main.h

--- main.h
+++ main.h
@@ -1,15 +1,45 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef PRINT_F
+#define PRINT_F
 
-/* Standard C library for header files */
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
-void getc_ints(int num);
-int putchers(char n);
-int putt_intts(int integer);
-int  p_printf(const char *format, ...);
-int putts(char *string);
-int m_put(char *string);
+/**
+* struct convert - This defines a structure for symbols
+* And functions to be executed
+* Author: kingsley
+* @sym: The operator
+* @f: The function associated
+*/
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
 
-#endif /* HEADER.H file */
+/*Function for main execution*/
+int piser(const char *format, conver_t f_list[], va_list args_ls);
+int _printf(const char *format, ...);
+int put_chars(char);
+int char_pr(va_list);
+int strs_pr(va_list);
+int perc_pr(va_list);
+int inte_pr(va_list);
+int nums_pr(va_list);
+int bin_pr(va_list);
+int rever_pr(va_list arg);
+int Aot13(va_list);
+int unsigned_intevalue(va_list);
+int octal_pr(va_list ls);
+int hex_pr(va_list list);
+int hex_prr(va_list list);
+unsigned int len_bas(unsigned int, int);
+char *str_rev(char *);
+void show_bas(char *str);
+char *cpmom(char *dest, char *src, unsigned int n);
+int unsgined_numpr(unsigned int);
+
+
+#endif
