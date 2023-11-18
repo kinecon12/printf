6cdf37fbbb371144a1e3d6c556223a82ebe81514
kinecon12 <kinecon12@gmail.com>
2023-11-17 17:47:40 -0800

    update

--- main.h
+++ main.h
@@ -1,45 +1,24 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+#include <stdint.h>
 
-/**
-* struct convert - This defines a structure for symbols
-* And functions to be executed
-* Author: kingsley
-* @sym: The operator
-* @f: The function associated
-*/
-struct convert
-{
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
 
-/*Function for main execution*/
-int piser(const char *format, conver_t f_list[], va_list args_ls);
+int _putchar(char c);
+int print_number(int n);
 int _printf(const char *format, ...);
-int put_chars(char);
-int char_pr(va_list);
-int strs_pr(va_list);
-int perc_pr(va_list);
-int inte_pr(va_list);
-int nums_pr(va_list);
-int bin_pr(va_list);
-int rever_pr(va_list arg);
-int Aot13(va_list);
-int unsigned_intevalue(va_list);
-int octal_pr(va_list ls);
-int hex_pr(va_list list);
-int hex_prr(va_list list);
-unsigned int len_bas(unsigned int, int);
-char *str_rev(char *);
-void show_bas(char *str);
-char *cpmom(char *dest, char *src, unsigned int n);
-int unsgined_numpr(unsigned int);
+int print_octal(unsigned int n);
+int print_unsig(unsigned int n);
+int print_hexaup(unsigned int n);
+int print_hexalow(unsigned int n);
+int print_bi(unsigned int n);
+int _printstring(char *s);
+int rot13(char *point);
+int print_rev(char *);
+int print_hl(uintptr_t n);
+void _puts(char *str);
 
 
 #endif
